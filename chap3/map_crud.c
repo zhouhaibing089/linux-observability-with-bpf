a1a82b501a7381411a03aacef86f4dfe9e2057af
zhouhaibing089 <zhouhaibing089@gmail.com>
2019-12-28 23:12:57 -0800

    update

--- chap3/map_crud.c
+++ chap3/map_crud.c
@@ -9,6 +9,8 @@ int bpf_create_map(enum bpf_map_type map_type, int key_size, int value_size,
                    int max_entries, __u32 map_flags);
 int bpf_map_update_elem(int fd, const void *key, const void *value,
                         __u64 flags);
+int bpf_map_lookup_elem(int fd, const void *key, void *value);
+int bpf_map_delete_elem(int fd, const void *key);
 
 int main(int argc, char **argv) {
   int fd, key, value, result;
@@ -44,6 +46,25 @@ int main(int argc, char **argv) {
     printf("Map updated with new element\n");
   } else {
     printf("Failed to update map with new value: %d %s\n", result,
+           strerror(errno));
+  }
+
+  // read
+  key = 1;
+  result = bpf_map_lookup_elem(fd, &key, &value);
+  if (result == 0) {
+    printf("Value read from the map: '%d'\n", value);
+  } else {
+    printf("Failed to read value from the map: %d %s\n", result,
+           strerror(errno));
+  }
+
+  // delete
+  result = bpf_map_delete_elem(fd, &key);
+  if (result == 0) {
+    printf("Element deleted from the map\n");
+  } else {
+    printf("Failed to delete element from the map: %d %s\n", result,
            strerror(errno));
   }
 }
